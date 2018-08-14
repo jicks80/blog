880064b18cb85e50820ecfa695505a3c7548c298
Jai Singh <jicks8004@gmail.com>
2018-08-14 23:06:27 +0530

    Second Commit error rectified

--- my_blogs/src/app.py
+++ my_blogs/src/app.py
@@ -6,11 +6,11 @@ from src.models.post import Article
 app = Flask(__name__)  # '__main__'
 app.secret_key = "jose"
 
-
+'''
 @app.route('/')
 def home_template():
-    return render_template('home.html')
-
+    return render_template('/html')
+'''
 
 @app.before_first_request
 def initialize_database():
@@ -18,24 +18,24 @@ def initialize_database():
 
 
 # frtch all blogs
-@app.route('/page/')
+@app.route('/')
 def page():
     blog = Article.fetch_all()
     return render_template('index.html', blog=blog)
 
 
 # fetch a  blog by article
-@app.route('/page/<string:user>')
+@app.route('/pages/<string:user>')
 def pages(user):
     blog = Article.fetch_cat(user)
     return render_template('index.html', blog=blog)
 
 
-@app.route('/category/<string:user1>')
+@app.route('/page/<string:user>')
 def pages1(user):
     blog = Article.fetch_article(user)
     return render_template('singlepost.html', blog=blog)
 
 
 if __name__ == '__main__':
-    app.run(port=4596, debug=True)
+    app.run(port=4590, debug=True)
